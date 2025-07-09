5254af023df8114147f973e5beb79167fd973df9
int16 <int16int8@gmail.com>
2025-07-09 22:15:07 +0800

    fix: image

--- src/util/loadimage.ts
+++ src/util/loadimage.ts
@@ -1,5 +1,8 @@
 const loadImageAsset = (url: string): string => {
     // return new URL(`../assets/`+url,import.meta.url).href
+    if (import.meta.env.VITE_IS_GITHUB_PAGES) {
+        return `/neco/`+url
+    }
     return `/`+url
 }
 
